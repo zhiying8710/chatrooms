febf2237fbab61124d9026082bacfe22fa92ddac
zhiying8710 <zhiying8710@hotmail.com>
2014-04-16 11:48:45 +0800

    first!

---
+++ public/js/chat_ui.js
@@ -0,0 +1,103 @@
+// binge@sf
+
+function divEscapedContentElement(message, style) {
+    return $('<div style="' + style + '"></div>').text(message);
+}
+
+function divSystemContentElement(message) {
+    return $('<div></div>').html('<i>' + message + '</i>');
+}
+
+function processUserInput(chatApp, socket) {
+    var message = $('#send-message').val();
+    var systemMessage;
+
+    if (message.charAt(0) === '/') {
+        systemMessage = chatApp.processCommand(message);
+        if (systemMessage) {
+            $('#messages').append(divSystemContentElement('<span style="color: blue;">' + systemMessage + '</span>'));
+        }
+    } else {
+        chatApp.sendMessage($('#room').text(), message);
+        message = 'You: ' + message;
+        $('#messages').append(divEscapedContentElement(message, 'color: blue;'));
+        $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+    }
+
+    $('#send-message').val('');
+}
+
+var socket = io.connect();
+$(document).ready(function() {
+    var chatApp = new Chat(socket);
+    socket.on('nameResult', function(result) {
+        var message = '';
+        if (result.success) {
+            message = '<span style="color: gray;">You are now known as ' + result.name + '.</span>';
+            $('#send-message').attr('placeholder', result.name);
+        } else {
+            messgae = '<span style="color: red;">' + result.message + '</span>';
+        }
+        $('#messages').append(divSystemContentElement(message));
+    });
+
+    socket.on('joinResult', function(result) {
+        $('#room').text(result.room);
+        $('#messages').append(divSystemContentElement('<span style="color: red;">' + 'Room changed.' + '</span>'));
+    });
+
+    socket.on('message', function(message) {
+        var newElement = $('<div style="' + message.style + '"></div>').text(message.text);
+        $('#messages').append(newElement);
+    });
+
+    socket.on('rooms', function(rooms) {
+        $('#room-list div').remove();
+        for ( var room in rooms) {
+            room = room.substring(1, room.length);
+            var style = '';
+            if (room != '') {
+                if(room == $('#room').text()) {
+                    room = room + ('(current)');
+                    style = 'color: red';
+                }
+                $('#room-list').append(divEscapedContentElement(room, style));
+            }
+        }
+
+        $('#room-list div').click(function() {
+            if($(this).text() == $('#room').text()) {
+                return;
+            }
+            chatApp.processCommand('/join ' + $(this).text());
+            $('#send-message').focus();
+        });
+    });
+
+    socket.on('usersInRoom', function(usersInRoom){
+         $('#usersInRoom-list div').remove();
+         for ( var i in usersInRoom) {
+             var nn = usersInRoom[i];
+             var style = '';
+             if(nn == $('#send-message').attr('placeholder')) {
+                 nn = nn + '(Yourself)';
+                 style = 'color: red;';
+             }
+             $('#usersInRoom-list').append(divEscapedContentElement(nn, style));
+         }
+    });
+
+    setInterval(function() {
+        socket.emit('rooms');
+    }, 1000);
+
+    setInterval(function() {
+        socket.emit('usersInRoom');
+    }, 1000);
+
+    $('#send-message').focus();
+
+    $('#send-button').click(function() {
+        processUserInput(chatApp, socket);
+    });
+});
---
+++ server.js
@@ -0,0 +1,59 @@
+// author: binge@sf
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+function err404(resp) {
+    resp.writeHead(404, {'Content-Type': 'text/plain'});
+    resp.write('Error 404: resource not found!');
+    resp.end();
+}
+
+function sendFile(resp, filePath, fileContent) {
+    resp.writeHead(200, {'content-type': mime.lookup(path.basename(filePath))});
+    resp.end(fileContent);
+}
+
+function serveStatic(resp, cache, absPath) {
+    if(cache[absPath]) {
+        sendFile(resp, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists){
+            if(exists) {
+                fs.readFile(absPath, function(err, data){
+                    if(err) {
+                        err404(resp);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(resp, absPath, data);
+                    }
+                });
+            } else {
+                err404(resp);
+            }
+        });
+    }
+}
+
+var server = http.createServer(function(req, resp){
+    var filePath = false;
+    if(req.url === '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + req.url;
+    }
+
+    var absPath = './' + filePath;
+    serveStatic(resp, cache, absPath);
+});
+
+server.listen(3000, function(){
+    console.log('server listening on port 3000.');
+});
+
+var chartServer = require('./lib/chat_server');
+chartServer.listen(server);
+
